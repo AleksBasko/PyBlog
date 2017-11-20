72d902b899902e6ef4a3942936af5f36e52bb206
Aleks Spiegel <aleks.spiegel@gmail.com>
2017-11-20 20:08:12 +0300

    style list page

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by user0 on 29.09.2017.
+ */
+
+console.log('first');
+
+$(document).ready(function() {
+   console.log('2423423');
+});
+/**
+ * Created by user0 on 29.09.2017.
+ */
+console.log('last')
