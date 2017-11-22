9dfd81b1c2962997a7ae1c3116608e8d222b9ec1
Aleks Spiegel <aleks.spiegel@gmail.com>
2017-11-22 22:06:39 +0300

    add style

--- static-src/src/js/main.js
+++ static-src/src/js/main.js
@@ -3,5 +3,20 @@
  */
 (function () {
     console.log('first');
+    showMenu();
 }());
 
+function showMenu() {
+    let btn = document.getElementById('menu-btn');
+    let menu = document.getElementById('menu-block');
+    let itemList = menu.querySelectorAll('.menu__item');
+
+    btn.addEventListener('click', function() {
+        console.log(itemList);
+        for(let i=0; i < itemList.length; i++) {
+            setTimeout(function(){
+                itemList[i].classList.add('active');
+            }, i * 1000);
+        }
+    })
+}
